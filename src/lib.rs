1d2f60eb0a6b16dcc03d580a310e5500b65dc398
wellscrosby <wellscrosby@gmail.com>
2026-08-20 14:55:27 -0800

    inital commit (needs cleanup)

--- src/lib.rs
+++ src/lib.rs
@@ -51,7 +51,9 @@ pub mod panel_focus;
 
 use std::{collections::BTreeMap, marker::PhantomData};
 
-pub use inspector::{EditorCategory, EditorDescription, EditorHidden, SkipSerialization};
+pub use inspector::{
+    EditorCategory, EditorDescription, EditorHidden, EditorPreview, SkipSerialization,
+};
 
 pub mod camera_preview;
 pub mod camera_settings;
@@ -115,6 +117,7 @@ pub mod scaffold;
 pub mod scene_io;
 pub mod scene_ops;
 pub mod scenes;
+pub mod schema_preview;
 pub mod screenshot;
 pub mod scrolling_log;
 pub mod sdk_paths;
@@ -175,7 +178,7 @@ pub mod prelude {
     pub use crate::windowing::{editor_window_plugin, primary_window_attributes};
     pub use crate::{
         AppState, DylibLoaderPlugin, EditorCategory, EditorCorePlugin, EditorDescription,
-        EditorHidden, ExtensionPlugin, JackdawEditorPlugins, SkipSerialization,
+        EditorHidden, EditorPreview, ExtensionPlugin, JackdawEditorPlugins, SkipSerialization,
     };
     pub use jackdaw_api::prelude::*;
 
@@ -418,6 +421,7 @@ impl Plugin for EditorCorePlugin {
         .add_plugins(panel_focus::PanelFocusPlugin)
         .add_plugins((
             viewport_overlays::ViewportOverlaysPlugin,
+            schema_preview::SchemaPreviewPlugin,
             view_modes::ViewModesPlugin,
             status_bar::StatusBarPlugin,
             build_panel::BuildPanelPlugin,
